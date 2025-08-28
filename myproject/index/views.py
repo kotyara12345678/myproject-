481c970c3e5583e4f27e204a7de0ae23dbe91fbf
kotyara12345678 <savva.toch@gmail.com>
2025-08-28 20:34:01 +0400

    Добавил index.html, исправил urls и views

--- myproject/index/views.py
+++ myproject/index/views.py
@@ -1,12 +1,6 @@
 from django.shortcuts import render
-from django.shortcuts import render
 from .models import Articles
 
 def index_page(request):
-    return render(request, 'index/index.html')
-
-
-def news_home(request):
-    articles = Articles.objects.order_by('-data')  # новые сначала нах
+    articles = Articles.objects.order_by('-data')  
     return render(request, 'index/index.html', {'articles': articles})
-
