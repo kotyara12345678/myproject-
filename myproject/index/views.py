78bf018b604a833f21db3f168d8825f5f092507b
kotyara12345678 <savva.toch@gmail.com>
2025-08-29 14:03:34 +0400

    исправил все баги

--- myproject/index/views.py
+++ myproject/index/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
-from .models import Articles, get_object_or_404
+from django.shortcuts import render, get_object_or_404
+from .models import Articles
 
 def index_page(request):
     articles = Articles.objects.order_by('-data')  
-    return render(request, 'index/index.html', {'articles': articles}),
+    return render(request, 'index/index.html', {'articles': articles})
 
 def article_detail(request, pk):
     article = get_object_or_404(Articles, pk=pk)
